9e76e27342ccf83c74220eba2b96e020a2d1b4df
julien-heng <heng.julien@hotmail.fr>
2026-02-08 00:03:15 -0500

    part 3

--- ECSE-420-Assignments/ca/mcgill/ecse420/a1/DiningPhilosophers.java
+++ ECSE-420-Assignments/ca/mcgill/ecse420/a1/DiningPhilosophers.java
@@ -9,20 +9,69 @@ public class DiningPhilosophers {
 	public static void main(String[] args) {
 
 		int numberOfPhilosophers = 5;
-                Philosopher[] philosophers = new Philosopher[numberOfPhilosophers];
-                Object[] chopsticks = new Object[numberOfPhilosophers];
+		Philosopher[] philosophers = new Philosopher[numberOfPhilosophers];
+		ReentrantLock[] chopsticks = new ReentrantLock[numberOfPhilosophers];
+
+		for (int i = 0; i < numberOfPhilosophers; i++) {
+			chopsticks[i] = new ReentrantLock(true);
+		}
+
+		for (int i = 0; i < numberOfPhilosophers; i++) {
+			if (i % 2 == 0) {
+				philosophers[i] = new Philosopher(chopsticks[i], chopsticks[(i + 1) % numberOfPhilosophers]);
+			} else {
+				philosophers[i] = new Philosopher(chopsticks[(i + 1) % numberOfPhilosophers], chopsticks[i]);
+			}
+		}
+
+		ExecutorService executor = Executors.newFixedThreadPool(numberOfPhilosophers);
+
+        for (int i = 0; i < numberOfPhilosophers; i++) {
+            executor.execute(philosophers[i]);
+        }
+
 	}
 
 	public static class Philosopher implements Runnable {
 
-		
+		private ReentrantLock firstChopstick;
+		private ReentrantLock secondChopstick;
+
+		public Philosopher(ReentrantLock first, ReentrantLock second) {
+			this.firstChopstick = first;
+			this.secondChopstick = second;
+		}
 
 		@Override
 		public void run() {
-			
+			while (true) {
+				try {
+					think();
+					firstChopstick.lock();
+					try {
+						secondChopstick.lock();
+						try {
+							eat();
+						} finally {
+							secondChopstick.unlock();
+						}
+					} finally {
+						firstChopstick.unlock();
+					}
+				} catch (InterruptedException e) {
+            		Thread.currentThread().interrupt();
+            		break;
+				}
+			}
 		}
 
+		private void think() throws InterruptedException {
+            Thread.sleep((int) (Math.random() * 10));
+        }
 
+        private void eat() throws InterruptedException {
+            Thread.sleep((int) (Math.random() * 10));
+        }
 	}
 
 }
