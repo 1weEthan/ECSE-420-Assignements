6e5d2fc8b0499b5567629748655ec074dcad1afc
1weEthan <wu.ethan002@gmail.com>
2026-02-10 20:23:30 -0500

    add code for q1

---
+++ ECSE-420-Assignments/ca/mcgill/ecse420/a1/matrixMultiplication/MatrixMultiplication.java
@@ -0,0 +1,122 @@
+package ca.mcgill.ecse420.a1.matrixMultiplication;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
+
+public class MatrixMultiplication {
+	
+	private static final int NUMBER_THREADS = 2; // snapdragon x plus
+	private static final int MATRIX_SIZE = 2000;
+
+	public static void main(String[] args) {
+		// Generate two random matrices, same size
+		double[][] a = generateRandomMatrix(MATRIX_SIZE, MATRIX_SIZE);
+		double[][] b = generateRandomMatrix(MATRIX_SIZE, MATRIX_SIZE);
+
+		//measure time of both sequential and parallel
+		long startTime = System.currentTimeMillis();
+		double[][] result = parallelMultiplyMatrix(a, b);
+		System.out.println(" parallel time with ", NUMBER_THREADS, "processors: ",System.currentTimeMillis() - startTime, "ms\n");
+
+		startTime = System.currentTimeMillis();
+		double[][] result = sequentialMultiplyMatrix(a, b);
+		System.out.println("sequential time: "System.currentTimeMillis() - startTime, "ms\n");
+	}
+
+	
+	/**
+	 * Returns the result of a sequential matrix multiplication
+	 * The two matrices are randomly generated
+	 * @param a is the first matrix
+	 * @param b is the second matrix
+	 * @return the result of the multiplication
+	 *
+	 *
+	 * Explain how you validate the method produces the correct results.
+	 * I iterated through the for loops with an example matrix to see if all the calculations were made corretly
+	 * I repeated for different size of matrix
+	 * */
+
+	public static double[][] sequentialMultiplyMatrix(double[][] a, double[][] b) {
+		double[][] result = new double[a.length][a.length]; //assuming square matrix
+		// implement a matrix multiplication sequentially:
+		if (a[0].length != b.length) {
+				throw new IllegalArgumentException("Matrix lengths do not match");
+		}
+		for (int i = 0; i < b[0].length; i++) {
+			for (int j = 0; j < a.length; j++) {
+				for (int k = 0; k < a[0].length; k++) {
+					result[i][j] += a[i][k] * b[k][j];
+				}
+			}
+		}
+		return result;
+
+	}
+	
+	/**
+	 * Returns the result of a concurrent matrix multiplication
+	 * The two matrices are randomly generated
+	 * @param a is the first matrix
+	 * @param b is the second matrix
+	 * @return the result of the multiplication
+	 * */
+	public static double[][] parallelMultiplyMatrix(double[][] a, double[][] b) {
+		double[][] result = new double[a.length][a.length]; //assuming square matrix
+
+		// each task handles a range of rows
+		int rowsPerTask = a.length / NUMBER_THREADS;
+		rowsPerTask = (int)Math.floor(rowsPerTask);
+
+		List<Callable<Object>> tasks = new ArrayList<>();
+
+		for (int i = 0; i < NUMBER_THREADS; i++) {
+			int startRow = i * rowsPerTask;
+			int endRow;
+			// last task picks up any remaining rows
+			if (i == NUMBER_THREADS - 1) {
+				// take everything until the end of the array
+				endRow = a.length;
+			} else {
+				endRow = (i + 1) * rowsPerTask;
+			}
+			MatrixTask aTask = new MatrixTask(a, b, result, startRow, endRow);
+			startRow = endRow;
+
+			tasks.add(Executors.callable(aTask));
+		}
+
+		ExecutorService threadPool = Executors.newFixedThreadPool(NUMBER_THREADS);
+		try {
+			threadPool.invokeAll(tasks);
+			threadPool.shutdown();
+			threadPool.awaitTermination(1, TimeUnit.MINUTES); // arbitrary large time to clean up
+		} catch(Exception e){
+			e.printStackTrace();
+			System.exit(1);
+		}
+		return result;
+
+	}
+        
+        /**
+         * Populates a matrix of given size with randomly generated integers between 0-10.
+         * @param numRows number of rows
+         * @param numCols number of cols
+         * @return matrix
+         */
+		private static double[][] generateRandomMatrix(int numRows, int numCols) {
+			double matrix[][] = new double[numRows][numCols];
+			for (int row = 0; row < numRows; row++) {
+				for (int col = 0; col < numCols; col++) {
+					matrix[row][col] = (double) ((int) (Math.random() * 10.0));
+				}
+			}
+			return matrix;
+		}
+	
+}
---
+++ ECSE-420-Assignments/ca/mcgill/ecse420/a1/matrixMultiplication/MatrixTask.java
@@ -0,0 +1,36 @@
+package ca.mcgill.ecse420.a1.matrixMultiplication;
+
+
+public class MatrixTask implements Runnable {
+
+    private double[][] a;
+    private double[][] b;
+    private double[][] result;
+    private final int startRow;
+    private final int endRow;
+
+    MatrixTask(double[][] a, double[][] b, double[][] result, int start, int end) {
+
+        this.a = a;
+        this.b = b;
+        this.result = result;
+
+        this.startRow = start;
+        this.endRow = end;
+    }
+
+    @Override
+    public void run() {
+        multiplyMatrix();
+    }
+    private void multiplyMatrix() {
+        int n = a.length;
+        for (int i = startRow; i < endRow; i++) { // Only loop through assigned rows
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    result[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+    }
+}
