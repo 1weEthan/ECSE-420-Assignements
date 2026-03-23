62070b72cc1dc245262086793a99f4bacf065815
1weEthan <wu.ethan002@gmail.com>
2026-03-23 15:40:32 -0400

    created filter lock file

---
+++ ECSE-420-Assignments/ca/mcgill/ecse420/a2/Filterlock.java
@@ -0,0 +1,10 @@
+package ca.mcgill.ecse420.a2;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.*;
+
+public class Filterlock {
+
+    public static void main(String[] args) {
+    }}
