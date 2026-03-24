c6d115c2ee54cf039dd809737c1493396c75ce7c
1weEthan <wu.ethan002@gmail.com>
2026-03-24 10:29:50 -0400

    filter lock implementation

--- ECSE-420-Assignments/ca/mcgill/ecse420/a2/Filterlock.java
+++ ECSE-420-Assignments/ca/mcgill/ecse420/a2/Filterlock.java
@@ -1,10 +1,37 @@
 package ca.mcgill.ecse420.a2;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.locks.*;
+import java.util.concurrent.atomic.AtomicIntegerArray;
 
 public class Filterlock {
 
-    public static void main(String[] args) {
-    }}
+    private final int n; // num threads
+    private final AtomicIntegerArray level;
+    private final AtomicIntegerArray victim;
+    
+    public Filterlock(int n) {
+        this.n = n;
+        this.level = new AtomicIntegerArray(n);
+        this.victim = new AtomicIntegerArray(n);
+    }
+
+
+    public void lock(int me) {
+        // Loop through all waiting rooms (1 to n-1)
+        for (int L = 1; L < n; L++) {  
+            level.set(me, L);      // Check into the room
+            victim.set(L, me);     
+        
+            // The Spin Loop: Check every other thread
+            for (int k = 0; k < n; k++) {
+                while ((k != me) && (level.get(k) >= L) && (victim.get(L) == me)) {
+                    //wait
+                }
+            }
+        }
+    }
+     public void unlock(int me) {
+        level.set(me, 0);
+     }
+    
+
+    }
